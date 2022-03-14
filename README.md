01064edd2c7c8c1eac820b8f7d56fb0429b94c59
Die4Ever <die4ever2005@gmail.com>
2022-03-14 03:23:27 -0500

    randomizing ride types stats and more (fixes issue #1)

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # rollercoaster-tycoon-randomizer
 RollerCoaster Tycoon Randomizer mod for RCT1 and RCT2 inside of OpenRCT2.
 
-Copy the rctrando.js file into the OpenRCT2 plugin folder, which is usually `%USERPROFILE%\Documents\OpenRCT2\plugin\`
+Copy the `rctrando.js` file into the OpenRCT2 plugin folder, which is usually `%USERPROFILE%\Documents\OpenRCT2\plugin\`
 
-Discord discussion here https://discord.gg/jjfKT9nYDR
+Discord discussion here https://discord.gg/jjfKT9nYDR or follow me on Twitter @Die4EverDM
--- rctrando.js
+++ rctrando.js
@@ -1,6 +1,6 @@
 
 var rando_name = 'RollerCoaster Tycoon Randomizer';
-var rando_version = '0.03';
+var rando_version = '0.04';
 var rando_authors = ['Die4Ever'];
 // ~~ forces JS to convert to int
 var globalseed = ~~0;
@@ -10,9 +10,16 @@ gen2 = ~~2147483643;
 gen1 = ~~(gen2/2);
 
 function main() {
-    var savedData = context.getParkStorage().getAll();
-    console.log("savedData", savedData);
-    if(savedData.seed) {
+    var savedData;
+    console.log(rando_name+" v"+rando_version+" starting...");
+    try {
+        savedData = context.getParkStorage().getAll();
+        if(savedData)
+            console.log("savedData", savedData);
+    } catch(e) {
+        printException('error checking savedData: ', e);
+    }
+    if(savedData && savedData.seed) {
         setGlobalSeed(savedData.seed);
         console.log("restored saved seed "+globalseed);
         SubscribeEvents();
@@ -20,7 +27,7 @@ function main() {
     else {
         initRando();
     }
-    console.log(rando_name+" v"+rando_version+" finished startup");
+    console.log(rando_name+" v"+rando_version+" finished startup with seed "+globalseed);
 }
 
 registerPlugin({
@@ -44,7 +51,14 @@ function initRando() {
         setGlobalSeed(context.getRandom(1, 999999 + 1));
     }
     context.sharedStorage.set("RCTRando.nextSeed", null);
-    context.getParkStorage().set("seed", globalseed);
+    if (typeof ui !== 'undefined') {
+        // pause game and open menu
+    }
+    try {
+        context.getParkStorage().set("seed", globalseed);
+    } catch(e) {
+        printException('error saving seed: ', e);
+    }
     console.log(rando_name+" v"+rando_version+" starting with seed "+globalseed +", api version "+context.apiVersion);
 
     RandomizePark();
@@ -67,7 +81,7 @@ function rng(min, max) {
     if(tseed < 0)
         tseed = ~~(-tseed);
     var ret = (tseed >>> 8) % max;
-    console.log("rng("+min+", "+max+") "+tseed+", "+ret);
+    //console.log("rng("+min+", "+max+") "+tseed+", "+ret);
     return ret;
 }
 
@@ -79,6 +93,10 @@ function setGlobalSeed(newSeed) {
 
 function setSeed(newSeed) {
     tseed = ~~newSeed;
+}
+
+function setLocalSeed(name) {
+    setSeed(globalseed + crc32(name));
 }
 
 function randomize(value, difficulty) {
@@ -102,51 +120,8 @@ function RandomizeParkFlag(name, difficulty) {
 }
 
 function RandomizePark() {
-    /* console.log(park);
-    {
-        cash: 100000,
-        rating: 500,
-        bankLoan: 100000,
-        maxBankLoan: 350000,
-        entranceFee: 0,
-        guests: 2,
-        suggestedGuestMaximum: 0,
-        guestGenerationProbability: 87,
-        guestInitialCash: 500,
-        guestInitialHappiness: 192,
-        guestInitialHunger: 220,
-        guestInitialThirst: 216,
-        value: 140,
-        companyValue: 140,
-        totalRideValueForMoney: 0,
-        totalAdmissions: 0,
-        totalIncomeFromAdmissions: 0,
-        landPrice: 750,
-        constructionRightsPrice: 400,
-        parkSize: 6280,
-        name: 'Crazy Castle',
-        messages: [],
-        casualtyPenalty: 0,
-        getFlag: [Native Function],
-        setFlag: [Native Function],
-        postMessage: [Native Function]
-    } */
-
-    /*  type ParkFlags =
-        "difficultGuestGeneration" |
-        "difficultParkRating" |
-        "forbidHighConstruction" |
-        "forbidLandscapeChanges" |
-        "forbidMarketingCampaigns" |
-        "forbidTreeRemoval" |
-        "freeParkEntry" |
-        "noMoney" |
-        "open" |
-        "preferLessIntenseRides" |
-        "preferMoreIntenseRides" |
-        "scenarioCompleteNameInput" |
-        "unlockAllPrices"; */
-
+    setLocalSeed('RandomizePark');
+    
     RandomizeParkFlag("difficultGuestGeneration", 1);
     RandomizeParkFlag("difficultParkRating", 1);
     RandomizeParkFlag("forbidHighConstruction", 1);
@@ -167,57 +142,11 @@ function RandomizePark() {
 }
 
 function RandomizeMap() {
-    /* console.log(map);
-    {
-        size: { x: 100, y: 100 },
-       numRides: 0,
-       numEntities: 10000,
-       rides: [],
-       getRide: [Native Function],
-       getTile: [Native Function],
-       getEntity: [Native Function],
-       getAllEntities: [Native Function],
-       createEntity: [Native Function]
-    } */
+    setLocalSeed('RandomizeMap');
 }
 
 function RandomizeScenario() {
-    /* console.log(scenario);
-    {
-        name: 'Crazy Castle',
-        details: 'You have inherited a large castle - Your job is to convert it into a small theme park.',
-        completedBy: '?',
-        filename: 'Crazy Castle.SC6',
-        parkRatingWarningDays: 0,
-        objective:
-        {
-            type: 'guestsBy',
-            guests: 1500,
-            year: 4,
-            excitement: 0,
-            monthlyIncome: 0,
-            parkValue: 0
-        },
-        status: 'inProgress',
-        completedCompanyValue: null,
-        companyValueRecord: 0
-    }
-    
-    type ScenarioObjectiveType =
-        "none" |
-        "guestsBy" |
-        "parkValueBy" |
-        "haveFun" |
-        "buildTheBest" |
-        "10Rollercoasters" |
-        "guestsAndRating" |
-        "monthlyRideIncome" |
-        "10RollercoastersLength" |
-        "finish5Rollercoasters" |
-        "repayLoanAndParkValue" |
-        "monthlyFoodIncome";
-    */
-
+    setLocalSeed('RandomizeScenario');
     if(scenario.objective.guests)
         scenario.objective.guests = randomize(scenario.objective.guests, 1);
     if(scenario.objective.excitement)
@@ -226,87 +155,42 @@ function RandomizeScenario() {
         scenario.objective.monthlyIncome = randomize(scenario.objective.monthlyIncome, 1);
     if(scenario.objective.parkValue)
         scenario.objective.parkValue = randomize(scenario.objective.parkValue, 1);
-    if(scenario.objective.year)
-        scenario.objective.year = randomize(scenario.objective.year, -1);
+    if(scenario.objective.year) // ceil because it's nice to lean towards longer scenarios? need to make other things more difficult then
+        scenario.objective.year = Math.ceil(randomize(scenario.objective.year, -1));
     
     console.log(scenario);
     console.log(scenario.objective);
 }
 
 function RandomizeClimate() {
-    /* console.log(climate);
-    {
-        type: 'warm',
-        current: {
-            weather: 'partiallyCloudy',
-            temperature: 17
-        },
-        future: {
-            weather: 'cloudy',
-            temperature: 12
-        }
-    }
-
-    type ClimateType =
-        "coolAndWet" |
-        "warm" |
-        "hotAndDry" |
-        "cold";
-    */
+    setLocalSeed('RandomizeClimate');
    // TODO: need setClimate function binding
 }
 
 function RandomizeRideTypes() {
-    // TODO: need bindings for ride types to be able to adjust their stats
-    // or maybe just do it by listening to the trackplace event?
+    for(var r in map.rides) {
+        RandomizeRide(map.rides[r].id);
+    }
 
     context.subscribe("ride.ratings.calculate", function(ratings) {
-        console.log('ride.ratings.calculate', ratings);
-        /* { 0:
-            { rideId: 2,
-            excitement: 121,
-            intensity: 60,
-            nausea: 75 } } */
-        //ratings.excitement = 999;
-        // modifying the ratings values here does work (or does it only modify the display?)
-        // but I want to do it in some meaningful way?
+        RandomizeRide(ratings.rideId);
     });
     context.subscribe("action.execute", function(event) {
         if( event.action != 'trackplace' ) return;
         if( event.isClientOnly ) return;
-        /*
-        {
-            action: 'trackplace',
-            args: {
-                x: 832,
-                y: 1536,
-                z: 144,
-                direction: 0,
-                ride: 2,
-                trackType: 261,
-                brakeSpeed: 0,
-                colour: 0,
-                seatRotation: 4,
-                trackPlaceFlags: 0,
-                isFromTrackDesign: false,
-                flags: 0
-            },
-            player: -1,
-            type: 3,
-            isClientOnly: false,
-            result: {
-                cost: 3970,
-                position: {
-                    x: 848,
-                    y: 1552,
-                    z: 144
-                },
-                expenditureType: 'ride_construction'
-            }
-        }
-        */
-        console.log('action.execute', event);
+        RandomizeRide(event.args.ride);
     });
+}
+
+function RandomizeRide(rideId) {
+    ride = map.getRide(rideId);
+    console.log('RandomizeRide '+ride.name+', type: '+ride.type);
+    setLocalSeed('RandomizeRide' + ride.type);
+    ride.runningCost = randomize(ride.runningCost, 1);
+    ride.inspectionInterval = randomize(ride.inspectionInterval, 1);
+    ride.excitement = randomize(ride.excitement, -1);
+    ride.intensity = randomize(ride.intensity, 0);
+    ride.nausea = randomize(ride.nausea, -1);
 }
 
 function RandomizeGuests() {
@@ -317,45 +201,38 @@ function RandomizeGuests() {
 }
 
 function RandomizeGuest(guest) {
-    /* https://github.com/OpenRCT2/OpenRCT2/blob/develop/distribution/openrct2.d.ts#L1366
-    console.log('RandomizeGuest:', guest);
-    RandomizeGuest': {
-        tshirtColour: 25,
-        trousersColour: 23,
-        balloonColour: 0,
-        hatColour: 0,
-        umbrellaColour: 0,
-        happiness: 170,
-        happinessTarget: 170,
-        nausea: 0,
-        nauseaTarget: 0,
-        hunger: 204,
-        thirst: 208,
-        toilet: 0,
-        mass: 51,
-        minIntensity: 0,
-        maxIntensity: 15,
-        nauseaTolerance: 3,
-        cash: 600,
-        isInPark: false,
-        isLost: true,
-        lostCountdown: 0,
-        peepType: 'guest',
-        name: 'Geoffrey D.',
-        destination:
-        { x: 0,
-            y: 0 },
-        energy: 72,
-        energyTarget: 72,
-        getFlag: [Native Function],
-        setFlag: [Native Function],
-        id: 66,
-        type: 'guest',
-        x: 1296,
-        y: 3263,
-        z: 112,
-        remove: [Native Function]
-    }
-    */
+    setLocalSeed('RandomizeGuest' + guest.id);
 }
 
+
+var crcTable = makeCRCTable();
+function makeCRCTable(){
+    var c;
+    var crcTable = [];
+    for(var n =0; n < 256; n++){
+        c = n;
+        for(var k =0; k < 8; k++){
+            c = ((c&1) ? (0xEDB88320 ^ (c >>> 1)) : (c >>> 1));
+        }
+        crcTable[n] = c;
+    }
+    return crcTable;
+}
+
+function crc32(str) {
+    //var crcTable = window.crcTable || (window.crcTable = makeCRCTable());
+    var crc = 0 ^ (-1);
+
+    for (var i = 0; i < str.length; i++ ) {
+        crc = (crc >>> 8) ^ crcTable[(crc ^ str.charCodeAt(i)) & 0xFF];
+    }
+
+    return (crc ^ (-1)) >>> 0;
+};
+
+function printException(msg, e) {
+    console.log('\nERROR:')
+    console.log(e.stack);
+    console.log(msg, e.name, e.message);
+    console.log('===========');
+}
