2e6501a5020c9ec7a888203036e2ece2d42a28b3
Die4Ever <die4ever2005@gmail.com>
2022-03-17 18:58:12 -0500

    GUI work in progress (issue #2)

--- rctrando.js
+++ rctrando.js
@@ -9,6 +9,9 @@ var gen1, gen2;
 gen2 = ~~2147483643;
 gen1 = ~~(gen2/2);
 
+var difficulty = 1;
+var scenarioLength = 0;// 0 means random
+
 function main() {
     var savedData;
     console.log(rando_name+" v"+rando_version+" starting...");
@@ -19,13 +22,29 @@ function main() {
     } catch(e) {
         printException('error checking savedData: ', e);
     }
+
     if(savedData && savedData.seed) {
         setGlobalSeed(savedData.seed);
         console.log("restored saved seed "+globalseed);
+        //initGui();// just for testing
         SubscribeEvents();
     }
     else {
-        initRando();
+        // saves in your Documents\OpenRCT2\plugin.store.json
+        var nextSeed = context.sharedStorage.get('RCTRando.nextSeed');
+        console.log("nextSeed", nextSeed);
+        if(nextSeed) {
+            setGlobalSeed(nextSeed);
+        } else {
+            setGlobalSeed(context.getRandom(1, 999999 + 1));
+        }
+        context.sharedStorage.set("RCTRando.nextSeed", null);
+        if (typeof ui !== 'undefined') {
+            // pause game and open menu
+            initGui();
+        } else {
+            initRando();
+        }
     }
     console.log(rando_name+" v"+rando_version+" finished startup with seed "+globalseed);
 }
@@ -42,31 +61,131 @@ registerPlugin({
 });
 
 function initRando() {
-    // saves in your Documents\OpenRCT2\plugin.store.json
-    var nextSeed = context.sharedStorage.get('RCTRando.nextSeed');
-    console.log("nextSeed", nextSeed);
-    if(nextSeed) {
-        setGlobalSeed(nextSeed);
-    } else {
-        setGlobalSeed(context.getRandom(1, 999999 + 1));
-    }
-    context.sharedStorage.set("RCTRando.nextSeed", null);
-    if (typeof ui !== 'undefined') {
-        // pause game and open menu
-    }
     try {
         context.getParkStorage().set("seed", globalseed);
     } catch(e) {
         printException('error saving seed: ', e);
     }
-    console.log(rando_name+" v"+rando_version+" starting with seed "+globalseed +", api version "+context.apiVersion);
+    console.log(rando_name+' v'+rando_version
+        + ' starting with seed '+globalseed
+        + ', api version '+context.apiVersion
+        + ', difficulty: '+difficulty
+        + ', scenarioLength: '+scenarioLength
+    );
 
-    RandomizePark();
-    RandomizeMap();
-    RandomizeClimate();
-    RandomizeScenario();
-    SubscribeEvents();
-    RandomizeGuests();
+    try {
+        RandomizeScenario();
+        RandomizePark();
+        RandomizeMap();
+        RandomizeClimate();
+        SubscribeEvents();
+        RandomizeGuests();
+    } catch(e) {
+        printException('error in initRando(): ', e);
+    }
+}
+
+function NewWidget(widget) {
+    var margin = 3;
+    var window_width = 300;
+    var start_y = 26;
+    // 3 margins because left, right, and center
+    cellWidth = (window_width - margin * 3) / 2;
+    cellHeight = 26;
+
+    widget.x *= cellWidth + margin;
+    widget.x += margin;
+    if(!widget.width)
+        widget.width = 1;
+    widget.width *= cellWidth;
+    widget.y *= cellHeight + margin*2;// double margins for y
+    widget.y += start_y;
+    widget.height = cellHeight;
+
+    return widget;
+}
+
+function NewLabel(label, desc) {
+    // copy instead of reference
+    desc = Object.assign({}, desc);
+    desc.type = 'label';
+    desc.name = 'label-' + desc.name;
+    desc.x = 0;
+    desc.text = label;
+    desc.textAlign = 'centred';
+    return NewWidget(desc);
+}
+
+function NewEdit(label, desc) {
+    label = NewLabel(label, desc);
+    desc.x = 1;
+    desc.y -= 0.2;
+    desc.type = 'textbox';
+    edit = NewWidget(desc);
+    return [label, edit];
+}
+
+function NewDropdown(label, desc) {
+    label = NewLabel(label, desc);
+    desc.x = 1;
+    desc.y -= 0.2;
+    desc.type = 'dropdown';
+    dropdown = NewWidget(desc);
+    return [label, dropdown];
+}
+
+function initGui() {
+    console.log('initGui()', globalseed);
+    var difficulties = {Easy: 0.5, Medium: 1, Hard: 2, Extreme: 3};
+    var scenarioLengths = {Speedrun: 0.5, Random: 0, Medium: 1, Long: 1.5, Marathon: 2};
+    var window = ui.openWindow({
+        classification: 'rando-settings',
+        title: "RollerCoaster Tycoon Randomizer",
+        width: 300,
+        height: 200,
+        widgets: [].concat(
+            NewLabel('https://discord.gg/jjfKT9nYDR', {
+                name: 'url',
+                y: 0,
+                width: 2,
+                tooltip: 'Join the Discord!'
+            }),
+            NewEdit('Seed:', {
+                name: 'edit-seed',
+                y: 1,
+                text: ''+globalseed,
+                tooltip: 'Enter a number'
+            }),
+            NewDropdown('Difficulty:', {
+                name: 'difficulty',
+                y: 2,
+                items: Object.keys(difficulties),
+                selectedIndex: 1,
+                tooltip: 'Choose a difficulty for the randomization'
+            }),
+            NewDropdown('Scenario Length:', {
+                name: 'length',
+                y: 3,
+                items: Object.keys(scenarioLengths),
+                selectedIndex: 1,
+                tooltip: 'Longer scenario length will also scale up the goals so that difficulty is maintained.'
+            })
+        ),
+        onClose: function() {
+            try {
+                var s = window.findWidget('edit-seed');
+                setGlobalSeed(s.text);
+                var d = window.findWidget('difficulty');
+                difficulty = difficulties[d.text];
+                var l = window.findWidget('length');
+                scenarioLength = scenarioLengths[l.text];
+                initRando();
+            } catch(e) {
+                printException('error in GUI onClose(): ', e);
+            }
+        }
+    });
+    return window;
 }
 
 function SubscribeEvents() {
@@ -87,6 +206,7 @@ function rng(min, max) {
 
 function setGlobalSeed(newSeed) {
     // use this to set the seed for the whole game
+    console.log('setGlobalSeed from '+globalseed+' to '+newSeed);
     globalseed = ~~newSeed;
     tseed = ~~newSeed;
 }
@@ -119,6 +239,36 @@ function RandomizeParkFlag(name, difficulty) {
     }
 }
 
+function RandomizeScenario() {
+    setLocalSeed('RandomizeScenarioLength');
+    if(scenarioLength == 0)
+        scenarioLength = rng(0.5, 3);
+        
+    console.log('scenario.objective.year: ', scenario.objective.year);
+    if(scenario.objective.year) {
+        // ceil because it's nice to lean towards longer scenarios? need to make other things more difficult then
+        scenario.objective.year = Math.ceil(scenario.objective.year * scenarioLength);
+        console.log('scenarioLength: '+scenarioLength+', new scenario.objective.year: '+scenario.objective.year);
+    } else {
+        // if we fail to adjust the scenario length, then we need to treat it as 1 so that the difficulty scaling isn't ruined
+        scenarioLength = 1;
+    }
+
+    setLocalSeed('RandomizeScenario');
+
+    if(scenario.objective.guests)
+        scenario.objective.guests = randomize(scenario.objective.guests, 1);
+    if(scenario.objective.excitement)
+        scenario.objective.excitement = randomize(scenario.objective.excitement, 1);
+    if(scenario.objective.monthlyIncome)
+        scenario.objective.monthlyIncome = randomize(scenario.objective.monthlyIncome, 1);
+    if(scenario.objective.parkValue)
+        scenario.objective.parkValue = randomize(scenario.objective.parkValue, 1);
+        
+    console.log(scenario);
+    console.log(scenario.objective);
+}
+
 function RandomizePark() {
     setLocalSeed('RandomizePark');
     
@@ -143,23 +293,6 @@ function RandomizePark() {
 
 function RandomizeMap() {
     setLocalSeed('RandomizeMap');
-}
-
-function RandomizeScenario() {
-    setLocalSeed('RandomizeScenario');
-    if(scenario.objective.guests)
-        scenario.objective.guests = randomize(scenario.objective.guests, 1);
-    if(scenario.objective.excitement)
-        scenario.objective.excitement = randomize(scenario.objective.excitement, 1);
-    if(scenario.objective.monthlyIncome)
-        scenario.objective.monthlyIncome = randomize(scenario.objective.monthlyIncome, 1);
-    if(scenario.objective.parkValue)
-        scenario.objective.parkValue = randomize(scenario.objective.parkValue, 1);
-    if(scenario.objective.year) // ceil because it's nice to lean towards longer scenarios? need to make other things more difficult then
-        scenario.objective.year = Math.ceil(randomize(scenario.objective.year, -1));
-    
-    console.log(scenario);
-    console.log(scenario.objective);
 }
 
 function RandomizeClimate() {
