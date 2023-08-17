c08f2917a82a2eeff0e3d530896d75d334db4da4
Crazycolbster <colbyhatton@gmail.com>
2023-08-17 18:47:51 +1200

    Added further debug functions

--- src/gui/archipelagoWindow.ts
+++ src/gui/archipelagoWindow.ts
@@ -537,7 +537,7 @@ function archipelagoDebug(){
                     type: 'button',
                     name: 'debug-button6',
                     x: 5,
-                    y: 140,
+                    y: 170,
                     width: 200,
                     height: 25,
                     text: 'Release Rule',
@@ -556,6 +556,72 @@ function archipelagoDebug(){
                         // settings.archipelago_park_message_chat = true;
                         var BathroomTrap = GetModule("RCTRArchipelago") as RCTRArchipelago;
                         BathroomTrap.ReleaseRule("forbidTreeRemoval");
+                        // park.setFlag("difficultGuestGeneration", false);
+                        // context.registerAction('ExplodeRide', (args) => {return {};}, (args) => explodeRide());
+                        // ac_req({"cmd":"PrintJSON","data":[{"text":"Cool1 (Team #1) has completed their goal."}],"type":"Goal","team":0,"slot":1})
+                        // console.log(context.getParkStorage().get("RCTRando.ArchipelagoPlayers"))
+                        // console.log(context.getParkStorage().get('RCTRando.nuttin'));
+                        // (BathroomTrap as RCTRArchipelagoConnection).connect();
+                        // init_archipelago_connection();
+                        
+                    }
+                },
+                {
+                    type: 'button',
+                    name: 'debug-button7',
+                    x: 5,
+                    y: 200,
+                    width: 200,
+                    height: 25,
+                    text: '"Grant Discount(Land)"',
+                    onClick: function() {
+                        // network.sendMessage("data.data.text");
+                        // console.log(RideType["Merry Go Round"]);
+                        
+                        // park.cash = 10000;
+                        // var i = "Monorail";
+                        //console.log(RideType["rollercoaster"]);
+                        //console.log(RideType[i]);
+                        // console.log(scenario.status);
+                        //park.setFlag("scenarioCompleteNameInput",true);
+                        //console.log(map.rides[0]);
+                        //console.log(RideType["Looping Roller Coaster"].rideType);
+                        // settings.archipelago_park_message_chat = true;
+                        var BathroomTrap = GetModule("RCTRArchipelago") as RCTRArchipelago;
+                        BathroomTrap.GrantDiscount("Land Discount");
+                        // park.setFlag("difficultGuestGeneration", false);
+                        // context.registerAction('ExplodeRide', (args) => {return {};}, (args) => explodeRide());
+                        // ac_req({"cmd":"PrintJSON","data":[{"text":"Cool1 (Team #1) has completed their goal."}],"type":"Goal","team":0,"slot":1})
+                        // console.log(context.getParkStorage().get("RCTRando.ArchipelagoPlayers"))
+                        // console.log(context.getParkStorage().get('RCTRando.nuttin'));
+                        // (BathroomTrap as RCTRArchipelagoConnection).connect();
+                        // init_archipelago_connection();
+                        
+                    }
+                },
+                {
+                    type: 'button',
+                    name: 'debug-button8',
+                    x: 5,
+                    y: 230,
+                    width: 200,
+                    height: 25,
+                    text: '"Grant Discount(Construction Rights)"',
+                    onClick: function() {
+                        // network.sendMessage("data.data.text");
+                        // console.log(RideType["Merry Go Round"]);
+                        
+                        // park.cash = 10000;
+                        // var i = "Monorail";
+                        //console.log(RideType["rollercoaster"]);
+                        //console.log(RideType[i]);
+                        // console.log(scenario.status);
+                        //park.setFlag("scenarioCompleteNameInput",true);
+                        //console.log(map.rides[0]);
+                        //console.log(RideType["Looping Roller Coaster"].rideType);
+                        // settings.archipelago_park_message_chat = true;
+                        var BathroomTrap = GetModule("RCTRArchipelago") as RCTRArchipelago;
+                        BathroomTrap.GrantDiscount("Construction Rights Discount");
                         // park.setFlag("difficultGuestGeneration", false);
                         // context.registerAction('ExplodeRide', (args) => {return {};}, (args) => explodeRide());
                         // ac_req({"cmd":"PrintJSON","data":[{"text":"Cool1 (Team #1) has completed their goal."}],"type":"Goal","team":0,"slot":1})
